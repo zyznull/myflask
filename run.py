c014fdaa13f96cdd23e4fcb7201d2e9753552ecd
zyz <362243277@qq.com>
2017-11-16 21:44:18 +0800

    new run

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import app
 from werkzeug.contrib.fixers import ProxyFix
 
-app.wsgi_app = ProxyFix(app.wsgi_app)
-app.run(host = '0.0.0.0',port = 8888,debug = True)
+if __name__ == '__main__':
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.run(host = '0.0.0.0',port = 8888,debug = True)
