4cfdf83ad53e56ef428eb73d1939e08026e35421
zyz <362243277@qq.com>
2017-11-19 10:31:37 +0800

    change run.py

--- run.py
+++ run.py
@@ -3,5 +3,4 @@ from app import app
 
 if __name__ == '__main__':
     app.wsgi_app = ProxyFix(app.wsgi_app)
-
-app.run(host = '0.0.0.0',port = 8888,debug = True)
+    app.run(host = '0.0.0.0',port = 8888,debug = True)
